7bcbbdbb1f8a613f094a912f3c53f8bb739a45fc
corey pfleiger <corey.c.pfleiger@gmail.com>
2022-04-06 12:02:36 -0400

    C Create made

--- AgileCampaign.Model/CampaignCreate.cs
+++ AgileCampaign.Model/CampaignCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace AgileCampaign.Model
 {
     public class CampaignCreate
     {
+        [Required]
+        [MinLength(3, ErrorMessage ="Give players a good type: Gritty, Rule of cool, Water World, Module:WaterDeep, ext")]
+        [MaxLength(30, ErrorMessage ="Ruel of thumb for type: Keep It Simple Silly")]
+        public string GameType { get; set; }
+        [Required]
+        [MinLength(4, ErrorMessage = "Type none if there are no restrictions, other wise Keep It Simple Silly (K.I.S.S)")]
+        [MaxLength(150, ErrorMessage = "Ruel of thumb for type: Keep It Simple Silly")]
+        public string Restrictions { get; set; }
+        [Required]
+        [MinLength(4, ErrorMessage = "Type none if you havent come up with one")]
+        [MaxLength(550, ErrorMessage = "Remember, K.I.S.S. this isnt a book!")]
+        public string Discription { get; set; }
+
+        [Required]
+        public int NumPlayers { get; set; }
+        [Required]
+        public int SessionLength { get; set; }
     }
 }
--- AgileCampaign.data/Campaign.cs
+++ AgileCampaign.data/Campaign.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace AgileCampaign.data
 {
     public class Campaign
     {
+        [Key]
+        public int GameId { get; set; }
+
+        [Required]
+        public int SessionLength { get; set; }
+        [Required]
+        public int NumPlayers { get; set; }
+        [Required]
+        public string GameType { get; set; }
+        [Required]
+        public string Restrictions { get; set; }
+        [Required]
+        public string Discription { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
     }
 }
