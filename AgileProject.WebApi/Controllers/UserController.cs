414379b91594ecde039ebee96c05467efda0b813
Victor Ryan <vryan9615@gmail.com>
2022-04-06 11:05:07 -0400

    Finished up create and get models for users

---
+++ AgileProject.WebApi/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using AgileUser.Models;
+using AgileUser.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AgileProject.WebApi.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        private UserService CreateUserService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var userService = new UserService(userId);
+            return userService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            UserService userService = CreateUserService();
+            var Users = userService.GetUsers();
+            return Ok(Users);
+        }
+
+        public IHttpActionResult Post(UserCreate user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateUserService();
+
+            if (!service.CreateUser(user))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
