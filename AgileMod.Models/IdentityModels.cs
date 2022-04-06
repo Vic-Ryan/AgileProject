18d697a86e5e00b95b597fbdf9afe38800ae4b0a
avery finchum <finchum.avery@gmail.com>
2022-04-06 14:41:21 -0400

    id models update

--- AgileMod.Models/IdentityModels.cs
+++ AgileMod.Models/IdentityModels.cs
@@ -1,10 +1,13 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 
-namespace AgileProject.WebApi.Models
+namespace AgileMod.Models
 {
     public class ApplicationUser : IdentityUser
     {
@@ -30,7 +33,7 @@ namespace AgileProject.WebApi.Models
         {
             return new ApplicationDbContext();
         }
-        public DbSet<Post> Posts { get; set; }
+        public DbSet<Mod> Mods { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder
